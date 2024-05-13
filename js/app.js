e59c06ce02b10f6cd4129880f3de8ea1be39f143
AaronM524 <aaronmathew468@gmail.com>
2024-05-13 10:42:36 -0400

    started code

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
 // CODE EXPLAINED channel
+const clear = document.querySelector(".clear");
+const dataElement = document.getElementById("date");
+const list = document.getElementById("list");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+const input = document.getElementById("input");
+
+const element = document.getElementById("element");
+element.insertAdjacentHTML(position, text);
+
+    "beforebegin"
+    "afterbegin"
+    "beforeend"
+    "afterend"
+
+element.innerHTML(text);
+
+
+
+
+const text = <li class="item">
+    <i class="co fa fa-circle-thin" job="complete"></i>
+    <p class="text"> Drink Coffee </p>
+    <i class="de fa fa-trash-o" job="delete"></i>
+</li>
+
+list. insertAdjacentHTML(position, text);
