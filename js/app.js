2c42391e47fcc261f4df5e172357c4fe23bbbfe5
AaronM524 <aaronmathew468@gmail.com>
2024-05-16 09:28:02 -0400

    work in progress

--- js/app.js
+++ js/app.js
@@ -16,11 +16,33 @@ element.innerHTML(text);
 
 
 
-
+function addToDo( toDo ) {
 const text = <li class="item">
     <i class="co fa fa-circle-thin" job="complete"></i>
     <p class="text"> Drink Coffee </p>
     <i class="de fa fa-trash-o" job="delete"></i>
 </li>
 
+const position = "beforeend";
+
 list. insertAdjacentHTML(position, text);
+}
+addToDo("Drink Coffee");
+
+document.addEventListener("keyup",function(){
+        if(event.keyCode == 13){
+            const toDo = input.value ;
+            if (toDo) {
+                addToDo();
+            }
+            input.value ="";
+        }
+
+});
+
+
+let LIST = [];
+let id = 0;
+LIST = [{} , {} ,  ];
+
+
